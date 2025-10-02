f6d0d98ad217148e84366fd7bceeda01cd161e1a
Pedr0 <elpedrorenan08@gmail.com>
2025-10-02 10:36:22 -0300

    excluiindo o venv do repositorio git não é necessario subir pra la

--- venv/lib/python3.12/site-packages/roboticstoolbox/examples/eg2.py
+++
@@ -1,3 +0,0 @@
-import roboticstoolbox as rtb
-robot = rtb.models.URDF.Puma560()
-robot.plot(robot.qn)
--- venv/lib/python3.12/site-packages/roboticstoolbox/examples/test1.py
+++
@@ -1,6 +0,0 @@
-from roboticstoolbox import *
-
-puma = models.DH.Puma560()
-
-q = jtraj(puma.qr, puma.qz, 50)
-puma.plot(q.q, movie='bob.gif', backend='pyplot')
